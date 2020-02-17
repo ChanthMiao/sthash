18f63a08626fb61050fd351eefce784988e45b87
ChanthMiao <shielde@outlook.com>
2020-02-17 22:23:10 +0800

    Update xmake config.

--- xmake.lua
+++ xmake.lua
@@ -4,6 +4,10 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("c99")
 
+set_warnings("all", "error")
+
+set_targetdir("$(projectdir)/build")
+
 if is_plat("windows") then
     add_cxflags("/source-charset: utf-8")
 end
@@ -22,6 +26,9 @@ target("sthash")
     add_files("src/*.c")
 
     add_links("mbedtls", "mbedcrypto")
+
+    set_default(true)
+
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
 --
